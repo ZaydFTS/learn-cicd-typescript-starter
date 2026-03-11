4ab80537dfb8d4e85102acb0fef33c0dc40a72f3
Zayd Falah <zayd.1falah@gmail.com>
2026-03-11 15:27:48 +0200

    Remove unused function.

--- src/main.ts
+++ src/main.ts
@@ -42,11 +42,6 @@ if (db) {
 
 v1Router.get("/healthz", handlerReadiness);
 
-function unused() {
-  // this function does nothing
-  // and is called nowhere
-}
-
 app.use("/v1", v1Router);
 
 app.listen(config.api.port, () => {
